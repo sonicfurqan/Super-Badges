8fe5af2ea273c8c1acc776ccb64b03886e116e5c
Syed Furqan Ahmed <sonicfurqan@gmail.com>
2022-09-17 19:33:48 +0530

    completing boatSearch

--- Lightning Web Components Specialist/force-app/main/default/lwc/boatSearch/boatSearch.js
+++ Lightning Web Components Specialist/force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -1,20 +1,34 @@
 import { LightningElement } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 
- // imports
- export default class BoatSearch extends NavigationMixin(LightningElement) {
+export default class BoatSearch extends NavigationMixin(LightningElement) {
     isLoading = false;
     
     // Handles loading event
-    handleLoading() { }
+    handleLoading() {
+        this.isLoading = true;
+    }
     
     // Handles done loading event
-    handleDoneLoading() { }
+    handleDoneLoading() {
+        this.isLoading = false;
+    }
     
     // Handles search boat event
     // This custom event comes from the form
-    searchBoats(event) { }
+    searchBoats(event) {
+        let boatTypeId = event.detail.boatTypeId;
+        this.template.querySelector('c-boat-search-results').searchBoats(boatTypeId);
+        this.handleDoneLoading();
+    }
     
-    createNewBoat() { }
-  }
-  
+    createNewBoat() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Boat__c',
+                actionName: 'new'
+            }
+        });        
+    }
+}
